c149dd269ada6fc5404df3967d3a974b7d29729c
CJ C <cjmills918@gmail.com>
2025-01-05 16:24:11 -0500

    started sensor grab function

--- src/main/java/frc/robot/subsystems/ClawGrab.java
+++ src/main/java/frc/robot/subsystems/ClawGrab.java
@@ -1,13 +1,19 @@
 package frc.robot.subsystems;
 
+import java.util.function.BooleanSupplier;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PneumaticsDevices;
+import frc.robot.Constants.dios;
 
 public class ClawGrab extends SubsystemBase {
     private static ClawGrab instance = null;
     private Solenoid m_clawSolenoid = new Solenoid(PneumaticsDevices.MODULE_TYPE, 
     PneumaticsDevices.CLAW_GRAB);
+
+    private final DigitalInput m_gamePieceSensor;
 
     public static ClawGrab getInstance() {
         if (instance == null) {
@@ -16,11 +22,19 @@ public class ClawGrab extends SubsystemBase {
         return instance;
     }
 
+    private ClawGrab() {
+        m_gamePieceSensor = new DigitalInput(dios.GAMEPIECE_SENSOR);
+    }   
+
     public void openClaw() {
         m_clawSolenoid.set(true);
     }
 
     public void closeClaw() {
         m_clawSolenoid.set(false);
+    }
+
+    public boolean gamePieceDetected() {
+        return !m_gamePieceSensor.get();
     }
 }
